c36f82d4768306a57d342d102236e146da8784cd
AJQuattropani <ajenningsquattrop99@unm.edu>
2025-05-26 16:08:34 -0600

    Added evergreen and background transparency to neovim.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,10 +1,10 @@
-
-vim.cmd("set tabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set expandtab")
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -23,7 +23,45 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {}
+local plugins = {
+  "neanias/everforest-nvim",
+  version = false,
+  lazy = false,
+  priority = 1000, -- make sure to load this before all the other start plugins
+  -- Optional; default configuration will be used if setup isn't called.
+  config = function()
+    require("everforest").setup({
+      -- Your config here
+      require("everforest").load(),
+      background=dark,
+      transparent_background_level = 100,
+      italics = true,
+      disable_italic_comments = false,
+      sign_column_background = "none",
+      ui_contrast = "high",
+      dim_inactive_windows = false,
+      diagnostic_text_highlist = true,
+      diagnostic_virtual_text = "coloured",
+      diagnostic_line_highlight = false,
+      spell_foreground = false,
+      show_sob = true,
+      float_style = "bright",
+      inlay_hints_background = "none",
+      --@param highlight_groups Highlights
+      --@param palette Palette
+      on_highlights = function(highlight_groups, palette) end,
+      ----
+      --@param palette Palette
+      colours_override = function(palette) end,
+      vim.cmd [[
+        highlight Normal guibg=none
+        highlight NonText guibg=none
+        highlight Normal ctermbg=none
+        highlight NonText ctermbg=none
+      ]]
+    })
+  end,
+}
 local opts = {}
-require("lazy").setup(plugin, opts)
+require("lazy").setup(plugins, opts)
 
